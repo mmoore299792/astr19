b2e187f0667121b0096e2788d7b637f4ac65b1eb
mmoore299792 <mmoore18@ucsc.edu>
2026-01-22 14:42:10 -0800

    Add files via upload

---
+++ journal2.py
@@ -0,0 +1,17 @@
+x = 7.0
+y = 2.0
+z = x + y
+print(z)
+print(type(z))
+
+m = 4
+n = 3
+o = m - n
+print(o)
+print(type(o))
+
+i = 5.0
+j = 6
+k = i*j
+print(k)
+print(type(k))
---
+++ journal3.py
@@ -0,0 +1,6 @@
+def f(x): 
+	return x**3 + 8
+print(f(9))
+
+if f(9) > 27:
+	print("YAY!")
